660a8f9ca7324856f77b19aeb6010114f2bee5bf
Luke Melia <luke@lukemelia.com>
2018-12-10 18:03:36 -0500

    Make preferRecognizer helpers search the entire parent hierarchy and return all matches.

--- addon/utils/recognizers.js
+++ addon/utils/recognizers.js
@@ -1,22 +1,20 @@
 export function preferRecognizer(component, recognizer) {
-  let parent = findParentWithProp(component, 'preferRecognizer');
-  if (parent) {
-    parent.preferRecognizer(recognizer);
-  }
+  let parents = findParentsWithProp(component, 'preferRecognizer');
+  parents.forEach((parent) => parent.preferRecognizer(recognizer));
 }
 
 export function stopPreferringRecognizer(component, recognizer) {
-  let parent = findParentWithProp(component, 'stopPreferringRecognizer');
-  if (parent) {
-    parent.stopPreferringRecognizer(recognizer);
-  }
+  let parents = findParentsWithProp(component, 'stopPreferringRecognizer');
+  parents.forEach((parent) => parent.stopPreferringRecognizer(recognizer));
 }
 
-function findParentWithProp(component, propName) {
+function findParentsWithProp(component, propName) {
+  let parents = [];
   let parentView  = component;
   while (parentView = parentView.parentView) { // eslint-disable-line no-cond-assign
     if (parentView[propName]) {
-      return parentView;
+      parents.push(parentView);
     }
   }
+  return parents;
 }
