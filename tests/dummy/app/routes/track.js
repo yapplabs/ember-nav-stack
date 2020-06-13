437aeb3fc41593df9967b75a060b228fbe8c8146
Luke Melia <luke@lukemelia.com>
2020-06-14 00:25:22 +0800

    Add missing track page title

--- tests/dummy/app/routes/track.js
+++ tests/dummy/app/routes/track.js
@@ -8,6 +8,7 @@ export default class extends YappDefaultRoute {
   get model() {
     return {
       slug: this.params.track_id,
+      pageTitle: 'Track',
       yapp: {
         myScheduleEnabled: true
       },
